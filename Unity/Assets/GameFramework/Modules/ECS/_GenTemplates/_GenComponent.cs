9ee840c56bbe2c38349d4ed2585c0201467977e9
Thomas Trocha <thomas.trocha@gmail.com>
2018-08-10 17:32:56 +0200

    Generator: Templates for component(system)-generation

---
+++ Unity/Assets/GameFramework/Modules/ECS/_GenTemplates/_GenComponent.cs
@@ -0,0 +1,40 @@
+
+using System ;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*name:using*/ /*endname*/
+
+/// <summary>
+/*name:componentComment*/ /*endname*/
+/// </summary>
+[System.Serializable]
+public partial class /*name:ComponentName*/GenTemplateComponent/*endname*/ : ECS.Component {
+/*block:enum*/
+    public enum /*name:enumName*/State/*endname*/ : int
+    {
+ /*block:entry*/        /*name:entryName*/state1/*endname*/ /*name:entryNumber*//*endname*/,
+ /*endblock:entry*/
+ /*block:rip*/        state2 = 1,
+        state3 = 2,
+ /*endblock:rip*/
+    }
+/*endblock:enum*/
+ 
+/*block:field*/
+/// <summary>
+/*name:comment*//*endname*/    
+/// </summary>
+    /*name:attributes*//*endname*/
+    public /*name:type*/State/*endname*/ /*name:name*/state/*endname*/;
+/*endblock:field*/
+}
+
+
+/*block:rip*/
+[System.Serializable]
+public class GenTemplateComponent2 : ECS.Component
+{
+    public float time;
+}
+/*endblock:rip*/
---
+++ Unity/Assets/GameFramework/Modules/ECS/_GenTemplates/_GenSystem.cs
@@ -0,0 +1,67 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using ECS;
+using UnityEngine;
+using UniRx;
+/*name:using*//*endname*/
+
+namespace Systems {
+/*block:basePart*/
+
+/// <summary>
+/*name:comment*//*endname*/
+/// </summary>
+
+
+    public partial class /*name:systemComponentsName*/GenTemplateSystemComponents/*endname*/ : ECS.ISystemComponents {
+        public UID Entity { get; set; }
+
+/*block:systemComponent*/        public /*name:componentName*/GenTemplateComponent/*endname*/ /*name:sysCompName*/templateComponent/*endname*/;
+/*endblock:systemComponent*/
+/*block:rip*/        public GenTemplateComponent2 comp2;/*endblock:rip*/
+    }
+
+    public class /*name:baseName*/GenTemplateSystemBase/*endname*/ : ECS.System</*name:systemComponentsName*/GenTemplateSystemComponents/*endname*/> {
+
+        protected override /*name:systemComponentsName*/GenTemplateSystemComponents/*endname*/ GetEntityComponents(/*name:systemComponentsName*/GenTemplateSystemComponents/*endname*/ components, UID entity) {
+/*block:getComponent*/            components./*name:sysCompName*/templateComponent/*endname*/ = GetComponent</*name:componentName*/GenTemplateComponent/*endname*/>(entity);
+/*endblock:getComponent*/
+/*block:rip*/            components.comp2 = GetComponent<GenTemplateComponent2>(entity);/*endblock:rip*/
+            return components;
+        }
+
+        protected override bool IsEntityValid(UID entity) {
+            if (/*block:rip*/HasComponent<GenTemplateComponent>(entity) /*endblock:rip*/
+                /*block:validComponent*//*name:OPERATION*/&&/*endname*/ HasComponent</*name:componentName*/GenTemplateComponent2/*endname*/>(entity) 
+/*endblock:validComponent*/
+             ) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+/*endblock:basePart*/
+/*block:implPart*/
+
+/// <summary>
+/*name:comment*//*endname*/
+/// </summary>
+    public partial class /*name:implName*/GenTemplateSystem/*endname*/ : /*name:baseName*/GenTemplateSystemBase/*endname*/
+    {
+
+        protected override void AfterBind() {
+            base.AfterBind();
+
+            Observable.EveryUpdate().Subscribe(e => ProcessAll()).AddTo(this);
+        }
+
+        protected override void Process(/*name:systemComponentsName*/GenTemplateSystemComponents/*endname*/ components) {
+            base.Process(components);
+            // add the system-logic here
+        }
+    }
+/*endblock:implPart*/
+}
