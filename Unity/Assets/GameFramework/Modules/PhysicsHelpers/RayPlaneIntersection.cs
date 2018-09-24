63cd57c2ac41ace72cade5e9180e4485407280a1
Wolfgang Reichardt <wolfgang@gentlymad.org>
2018-09-24 13:22:15 +0200

    - Added ray -> plane physics helper code

---
+++ Unity/Assets/GameFramework/Modules/PhysicsHelpers/RayPlaneIntersection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PhysicsHelper {
+    public class RayPlaneIntersection {
+        public static Vector3 CalculateIntersection(Vector3 planeOrigin, Vector3 planeNormal, Vector3 rayOrigin, Vector3 rayDirection) {
+            //Ray-> groundplane intersection
+            float distance = Vector3.Dot(planeNormal, planeOrigin - rayOrigin) / Vector3.Dot(planeNormal, rayDirection);
+            Vector3 intersection = rayOrigin + (rayDirection * distance);
+            return intersection;
+        }
+
+        public static Vector3 CalculateIntersection(Vector3 planeOrigin, Vector3 planeNormal, Ray ray) {
+            return CalculateIntersection(planeOrigin, planeNormal, ray.origin, ray.direction);
+        }
+    }
+}
