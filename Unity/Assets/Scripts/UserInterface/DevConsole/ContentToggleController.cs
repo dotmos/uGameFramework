3361564e3f109cc724c81bd1605899eec1dca964
Stephan Wirth <stephan@gentlymad.org>
2018-11-13 10:58:52 +0100

    DevelopmentConsole now saves it's position and content state (not size though) to PlayerPrefs!

---
+++ Unity/Assets/Scripts/UserInterface/DevConsole/ContentToggleController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UserInterface {
+    public class ContentToggleController : MonoBehaviour {
+        public GMToggle leftContentToggle;
+        public GameObject leftContent;
+        [Space]
+        public GMToggle rightContentToggle;
+        public GameObject rightContent;
+
+        private void Awake() {
+            leftContent.SetActive(PlayerPrefs.GetInt("DevelopmentConsole_IsLeftContentActive") == 0 ? false : true);
+            leftContent.SetActive(PlayerPrefs.GetInt("DevelopmentConsole_IsRightContentActive") == 0 ? false : true);
+
+            //Fallback for initialization of player prefs
+            if (!leftContent.activeSelf && !rightContent.activeSelf) {
+                ToggleLeftContent(true);
+                ToggleRightContent(true);
+            }
+
+            leftContentToggle.onValueChanged.AddListener(ToggleLeftContent);
+            rightContentToggle.onValueChanged.AddListener(ToggleRightContent);
+        }
+
+        void ToggleLeftContent(bool activate) {
+            leftContent.SetActive(activate);
+            rightContentToggle.interactable = activate;
+
+            PlayerPrefs.SetInt("DevelopmentConsole_IsLeftContentActive", activate ? 1 : 0);
+        }
+
+        void ToggleRightContent(bool activate) {
+            rightContent.SetActive(activate);
+            rightContentToggle.interactable = activate;
+
+            PlayerPrefs.SetInt("DevelopmentConsole_IsRightContentActive", activate ? 1 : 0);
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/UserInterface/DevConsole/SaveDevelopmentConsolePosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UserInterface {
+    [RequireComponent(typeof(GMDraggableObject))]
+    public class SaveDevelopmentConsolePosition : MonoBehaviour {
+        private GMDraggableObject dragHandler;
+
+        void Awake() {
+            dragHandler = GetComponent<GMDraggableObject>();
+            Vector2 restoredPosition = new Vector2(PlayerPrefs.GetFloat("DevelopmentConsole_PositionX"), PlayerPrefs.GetFloat("DevelopmentConsole_PositionY"));
+            dragHandler.target.anchoredPosition = restoredPosition;
+        }
+
+        private void OnApplicationQuit() {
+            PlayerPrefs.SetFloat("DevelopmentConsole_PositionX", dragHandler.target.anchoredPosition.x);
+            PlayerPrefs.SetFloat("DevelopmentConsole_PositionY", dragHandler.target.anchoredPosition.y);
+        }
+    }
+}
