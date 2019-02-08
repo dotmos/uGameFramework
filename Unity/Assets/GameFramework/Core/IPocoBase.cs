cc6c8a8bc43ca7be711ccf86c1ae6b0d151f8edc
Wolfgang Reichardt <wolfgang@gentlymad.org>
2019-02-08 13:03:53 +0100

    - Create IPocoBase. Should be used with custom classes that should work with the eclipse plugin, but were not defined with the eclipse plugin.

---
+++ Unity/Assets/GameFramework/Core/IPocoBase.cs
@@ -0,0 +1,7 @@
+﻿/// <summary>
+/// Interface for classes that should work with the eclipse plugin
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public interface IPocoBase<T> {
+    void ShallowCopy(T from);
+}
