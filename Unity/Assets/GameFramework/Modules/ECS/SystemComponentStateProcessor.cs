c69374f0b6bba93e200e1ce855723fb047eeb6c8
Wolfgang Reichardt <wolfgang@gentlymad.org>
2019-02-14 11:35:23 +0100

    - Created SystemComponentStateProcessor to help processing state based component actions

---
+++ Unity/Assets/GameFramework/Modules/ECS/SystemComponentStateProcessor.cs
@@ -0,0 +1,28 @@
+﻿namespace ECS {
+
+    /// <summary>
+    /// Simple state based systemcomponents processor
+    /// </summary>
+    /// <typeparam name="TSystemComponents"></typeparam>
+    /// <typeparam name="TStates"></typeparam>
+    public abstract class SystemComponentStateProcessor<TSystemComponents, TStates> where TSystemComponents : ISystemComponents {
+
+        public SystemComponentStateProcessor() {
+            Kernel.Instance.Inject(this);
+        }
+
+        protected abstract void OnEnter(TSystemComponents components, TStates oldState, TStates newState, float deltaTime);
+
+        protected abstract void OnExit(TSystemComponents components, TStates oldState, TStates newState, float deltaTime);
+
+        protected abstract TStates OnProcess(TSystemComponents components, TStates state, float deltaTime);
+
+        public void Process(TSystemComponents components, TStates state, float deltaTime) {
+            TStates newState = OnProcess(components, state, deltaTime);
+            if (!state.Equals(newState)) {
+                OnExit(components, state, newState, deltaTime);
+                OnEnter(components, state, newState, deltaTime);
+            }
+        }
+    }
+}
