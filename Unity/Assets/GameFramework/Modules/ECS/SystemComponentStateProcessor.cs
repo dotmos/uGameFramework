9b47ff19addd22db66452860ca82d85535302681
Wolfgang Reichardt <wolfgang@gentlymad.org>
2019-02-14 12:08:37 +0100

    - Refactored SystemComponentStateProcessor

--- Unity/Assets/GameFramework/Modules/ECS/SystemComponentStateProcessor.cs
+++ Unity/Assets/GameFramework/Modules/ECS/SystemComponentStateProcessor.cs
@@ -1,4 +1,6 @@
-﻿namespace ECS {
+﻿using System;
+
+namespace ECS {
 
     /// <summary>
     /// Simple state based systemcomponents processor
@@ -7,9 +9,19 @@
     /// <typeparam name="TStates"></typeparam>
     public abstract class SystemComponentStateProcessor<TSystemComponents, TStates> where TSystemComponents : ISystemComponents {
 
-        public SystemComponentStateProcessor() {
+        public delegate TStates GetState(TSystemComponents components);
+        GetState getState;
+
+        public delegate void SetState(TSystemComponents components, TStates state);
+        SetState setState;
+
+        public SystemComponentStateProcessor(GetState getState, SetState setState) {
             Kernel.Instance.Inject(this);
+
+            this.getState = getState;
+            this.setState = setState;
         }
+
 
         protected abstract void OnEnter(TSystemComponents components, TStates oldState, TStates newState, float deltaTime);
 
@@ -17,10 +29,12 @@
 
         protected abstract TStates OnProcess(TSystemComponents components, TStates state, float deltaTime);
 
-        public void Process(TSystemComponents components, TStates state, float deltaTime) {
+        public void Process(TSystemComponents components, float deltaTime) {
+            TStates state = getState(components);
             TStates newState = OnProcess(components, state, deltaTime);
             if (!state.Equals(newState)) {
                 OnExit(components, state, newState, deltaTime);
+                setState(components, newState);
                 OnEnter(components, state, newState, deltaTime);
             }
         }
