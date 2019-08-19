351c41e82822e10f7b483144003901a7ea2aafac
Wolfgang Reichardt <wolfgang@gentlymad.org>
2019-08-19 15:26:20 +0200

    - Created ThreadsafeRandom.cs

---
+++ Unity/Assets/GameFramework/Modules/ThreadsafeRandom/ThreadsafeRandom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class ThreadsafeRandom {
+    private static Random _global = new Random();
+    [ThreadStatic]
+    private static Random _local;
+
+    public static int Next(int minValue, int maxValue) {
+        Random inst = _local;
+        if (inst == null) {
+            int seed;
+            lock (_global) seed = _global.Next();
+            _local = inst = new Random(seed);
+        }
+        return inst.Next(minValue, maxValue);
+    }
+}
