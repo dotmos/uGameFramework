58dc7f862f7ec429bef1075a7354df14a8058d01
Stephan Wirth <stephan@gentlymad.org>
2019-10-08 13:08:26 +0200

    DevConsole is now protected by a password

---
+++ Unity/Assets/Scripts/UserInterface/DevConsole/Controller/ConsolePasswordController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UserInterface {
+    public class ConsolePasswordController : MonoBehaviour
+    {
+        public GMInputField passwordInput;
+        public GMButton applyButton;
+        /// <summary>
+        /// Define the current password here
+        /// </summary>
+        public string currentPassword;
+
+        private void Awake() {
+            applyButton.onClick.AddListener(CheckPassword);
+        }
+
+        void Update() {
+            if (Input.GetKeyUp(KeyCode.Return)) {
+                CheckPassword();
+            }
+        }
+
+        void CheckPassword() {
+            if (passwordInput.text == currentPassword) {
+                UnlockConsole();
+            }
+        }
+
+        public void LockConsole() {
+            passwordInput.text = "";
+            gameObject.SetActive(true);
+        }
+
+        public void UnlockConsole() {
+            gameObject.SetActive(false);
+        }
+    }
+}
