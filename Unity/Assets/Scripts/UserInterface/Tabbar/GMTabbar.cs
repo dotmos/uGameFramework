262ef027dac76f30cb51eba107306f9711118653
Stephan Wirth <stephan@gentlymad.org>
2019-11-21 14:56:47 +0100

    GMTabbar now checks whether a tab is already registered before registring it

--- Unity/Assets/Scripts/UserInterface/Tabbar/GMTabbar.cs
+++ Unity/Assets/Scripts/UserInterface/Tabbar/GMTabbar.cs
@@ -118,12 +118,14 @@ namespace UserInterface {
         /// <param name="tab">Tab.</param>
         public void RegisterTab(GMTab tab)
         {
-            tabs.Add(tab);
-            tab.Initialize(this);
-            RegisterToggle(tab);
+            if (!tabs.Contains(tab)) {
+                tabs.Add(tab);
+                tab.Initialize(this);
+                RegisterToggle(tab);
 
-            if (activateNewTabsOnAdd) {
-                ActivateTab(tab);
+                if (activateNewTabsOnAdd) {
+                    ActivateTab(tab);
+                }
             }
         }
 
