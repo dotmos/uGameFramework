cc8806f68b01f0e3ea2f124947e0f872df20f25d
Matthias Guntrum <m.guntrum@gentlymadstudios.com>
2020-01-23 13:44:57 +0100

    Added workaround for EnumFlagsAttribute when editing/selecting multiple objects at once, to prevent data being accidentally overwritten.

--- Unity/Assets/GameFramework/Extensions/Enum/EnumFlagsAttribute.cs
+++ Unity/Assets/GameFramework/Extensions/Enum/EnumFlagsAttribute.cs
@@ -13,7 +13,19 @@ public class EnumFlagsAttribute : PropertyAttribute {
 [CustomPropertyDrawer(typeof(EnumFlagsAttribute))]
 public class EnumFlagsAttributeDrawer : PropertyDrawer {
     public override void OnGUI(Rect _position, SerializedProperty _property, GUIContent _label) {
-        _property.intValue = EditorGUI.MaskField(_position, _label, _property.intValue, _property.enumNames);
+        // Workaround for multi edit: https://answers.unity.com/questions/1150282/how-to-correctly-use-editorguilayoutmaskfield-in-m.html
+        // To avoid wrongly setting/changing data
+        if (_property.hasMultipleDifferentValues) {
+            bool guienabled = GUI.enabled;
+            GUI.enabled = false;
+            EditorGUI.TextField(_position, _property.displayName, "Multi edit not allowed!");
+            GUI.enabled = guienabled;
+        } else {
+            int newVal = EditorGUI.MaskField(_position, _label, _property.intValue, _property.enumNames);
+            if (newVal != _property.intValue) {
+                _property.intValue = newVal;
+            }
+        }
     }
 }
 #endif
