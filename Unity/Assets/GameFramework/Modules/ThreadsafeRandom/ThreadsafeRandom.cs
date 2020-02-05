be8fece83c0bee7e6dfe3e2ff82c0daed1602b86
Wolfgang Reichardt <wolfgang@gentlymad.org>
2020-02-05 16:42:36 +0100

    - Create threadsafe Random.Range(float, float)

--- Unity/Assets/GameFramework/Modules/ThreadsafeRandom/ThreadsafeRandom.cs
+++ Unity/Assets/GameFramework/Modules/ThreadsafeRandom/ThreadsafeRandom.cs
@@ -5,7 +5,17 @@ public static class ThreadsafeRandom {
     [ThreadStatic]
     private static Random _local;
 
-    public static int Next(int minValue, int maxValue) {
+    public static int Next() {
+        Random inst = _local;
+        if (inst == null) {
+            int seed;
+            lock (_global) seed = _global.Next();
+            _local = inst = new Random(seed);
+        }
+        return inst.Next();
+    }
+
+    public static int Range(int minValue, int maxValue) {
         Random inst = _local;
         if (inst == null) {
             int seed;
@@ -13,5 +23,9 @@ public static class ThreadsafeRandom {
             _local = inst = new Random(seed);
         }
         return inst.Next(minValue, maxValue);
+    }
+
+    public static float Range(float minValue, float maxValue) {
+        return Range((int)(minValue * 1000), (int)(maxValue * 1000)) * 0.001f;
     }
 }
