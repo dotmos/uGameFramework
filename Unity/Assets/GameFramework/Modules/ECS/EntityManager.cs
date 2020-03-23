32c771e826a19ec77f4c2883cb634cfec412f113
Wolfgang Reichardt <wolfgang@gentlymad.org>
2020-03-23 13:19:37 +0100

    - Added try/catch to EntityManager.Tick content

--- Unity/Assets/GameFramework/Modules/ECS/EntityManager.cs
+++ Unity/Assets/GameFramework/Modules/ECS/EntityManager.cs
@@ -110,7 +110,7 @@ namespace ECS {
                 watchOverall.Restart();
 #endif
                 for (int i = 0; i < _systems.Count; ++i) {
-                    _systems[i].ProcessSystem(deltaTime);
+                    try { _systems[i].ProcessSystem(deltaTime); } catch (Exception e) { UnityEngine.Debug.LogError(e); }
                 }
 #if ECS_PROFILING && UNITY_EDITOR
                 watchOverall.Stop();
