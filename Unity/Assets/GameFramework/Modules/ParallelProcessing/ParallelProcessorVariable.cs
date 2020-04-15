a53ed9af511d42edcfdc17b81471f35f2503a4d5
Wolfgang Reichardt <wolfgang@gentlymad.org>
2020-04-15 14:09:09 +0200

    - Created ParallelProcessorVariable

---
+++ Unity/Assets/GameFramework/Modules/ParallelProcessing/ParallelProcessorVariable.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+namespace ParallelProcessing {
+    public class ParallelProcessorVariable<T> {
+        /// <summary>
+        /// LUT for accessing a value based on the thread id.
+        /// </summary>
+        Dictionary<int, T> threadVariableLUT;
+
+        /// <summary>
+        /// Cached thread IDs for garbage free LUT iterating
+        /// </summary>
+        List<int> cachedThreadIDs = new List<int>();
+
+        /// <summary>
+        /// If otherThreads is supplied, otherThreads will also get values in addition to the ParallelProcessorWorker threads
+        /// </summary>
+        /// <param name="mainThread"></param>
+        public ParallelProcessorVariable(Thread[] otherThreads = null) : this(default, otherThreads) { }
+
+        public ParallelProcessorVariable(T initialValue, Thread[] otherThreads = null) {
+            ParallelProcessorWorkers.Setup();
+
+            threadVariableLUT = new Dictionary<int, T>(ParallelProcessorWorkers.WorkerCount);
+            
+            //Create worker thread lists
+            foreach (Thread t in ParallelProcessorWorkers.Workers) {
+                threadVariableLUT[t.ManagedThreadId] = initialValue;
+                cachedThreadIDs.Add(t.ManagedThreadId);
+            }
+            //Also create lists for other threads?
+            if (otherThreads != null) {
+                foreach (Thread t in otherThreads) {
+                    threadVariableLUT[t.ManagedThreadId] = initialValue;
+                    cachedThreadIDs.Add(t.ManagedThreadId);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the value that is being used by the thread
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public T GetValueForThread(Thread t) {
+            return GetValueForThreadId(t.ManagedThreadId);
+        }
+
+        /// <summary>
+        /// Returns the value that is being used by the thread
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public T GetValueForThreadId(int id) {
+            return threadVariableLUT[id];
+        }
+
+        /// <summary>
+        /// Returns the value that is being used by the current thread
+        /// </summary>
+        /// <returns></returns>
+        public T GetValueForCurrentThread() {
+            return threadVariableLUT[Thread.CurrentThread.ManagedThreadId];
+        }
+
+        /// <summary>
+        /// Set the value for the current thread
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetValueForCurrentThread(T value) {
+            threadVariableLUT[Thread.CurrentThread.ManagedThreadId] = value;
+        }
+
+        /// <summary>
+        /// Returns all values
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<int, T>.ValueCollection GetAllValues() {
+            return threadVariableLUT.Values;
+        }
+
+        /// <summary>
+        /// Do an action on all values
+        /// </summary>
+        /// <param name="valueAction"></param>
+        public void OperateOnValues(Action<T> valueAction) {
+            for(int i=0; i<cachedThreadIDs.Count; ++i) {
+                valueAction(threadVariableLUT[cachedThreadIDs[i]]);
+            }
+        }
+        
+
+        /// <summary>
+        /// Clears all values to their default value
+        /// </summary>
+        public void DefaultAllValues() {
+            SetAllValues(default);
+        }
+
+        /// <summary>
+        /// Set all values
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetAllValues(T value) {
+            for(int i=0; i<cachedThreadIDs.Count; ++i){
+                threadVariableLUT[cachedThreadIDs[i]] = value;
+            }
+        }        
+    }
+}
