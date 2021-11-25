f13765967bf1e9cb2f64d5ad80dda14a52c932f6
Stephan Wirth <stephan@gentlymad.org>
2021-11-25 11:14:50 +0100

    Introduced GMSelectable as a custom base class that should from now on be used instead of unity's Selectable * GMbutton, GMToggle, GMInputfield, GMScrollbar and GMTab now have their IsInteractable method overridden to not be a neighbour to another selectable if they are not parented to any other object. This fixes the bug, that sometimes a UI element that is returned to the root of the scene (e.g. by an object pool) can be selected even though it is not visible to the player.

--- Unity/Assets/Scripts/UserInterface/Basics/GMButton.cs
+++ Unity/Assets/Scripts/UserInterface/Basics/GMButton.cs
@@ -140,5 +140,12 @@ namespace UserInterface {
                 }
             }
         }
+
+        public override bool IsInteractable() {
+            //Is Only interactable if it has a parent (thus is part of a canvas).
+            //This is used to avoid that it is considered for navigation creation
+            //event though it has been returned (to root) by an object pool
+            return base.IsInteractable() && transform.parent != null;
+        }
     }
 }
---
+++ Unity/Assets/Scripts/UserInterface/Basics/GMSelectable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.UI;
+
+namespace UserInterface {
+    public class GMSelectable : Selectable
+    {
+        public override bool IsInteractable() {
+            //Is Only interactable if it has a parent (thus is part of a canvas).
+            //This is used to avoid that it is considered for navigation creation
+            //event though it has been returned (to root) by an object pool
+            return base.IsInteractable() && transform.parent != null;
+        }
+    }
+}
